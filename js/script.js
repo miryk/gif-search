1d0137689fd9b4f023d79292e043e255d48e4657
Miry Konishi <miry.konishi@gmail.com>
2023-01-02 17:53:59 -0300

    API request and injecting results to DOM

--- js/script.js
+++ js/script.js
@@ -1,22 +1,25 @@
-var searchInputData = $("#keyword").text();
-console.log(searchInputData);
+$("#btn-buscar").click(function () {
+  var searchInputData = $("#input-buscar").val();
+  // cambia espacios en "+" si hay:
+  var searchWord = searchInputData;
+  searchWord = searchWord.split(" ");
+  searchWord = searchWord.join("+");
 
-var searchWord = searchInputData;
-searchWord = "cat";
-
-// for test
-$.ajax({
-  url: `https://api.giphy.com/v1/gifs/search?api_key=C5cpMfxUdMXpWh2wFPR0W66BGmo2n65f&q=${searchWord}&limit=10&offset=0&rating=g&lang=en`,
-}).done( function (results) {
-  console.log(results.data);
-});
-
-
-
-$("#search").click(function () {
+  $("#resultados-busqueda").prepend(`
+      <div class="results history">
+      `)
   $.ajax({
-    url: `https://api.giphy.com/v1/gifs/search?api_key=C5cpMfxUdMXpWh2wFPR0W66BGmo2n65f&q=${searchWord}&limit=10&offset=0&rating=g&lang=en`,
+    url: `https://api.giphy.com/v1/gifs/search?api_key=C5cpMfxUdMXpWh2wFPR0W66BGmo2n65f&q=${searchWord}&limit=10&offset=0&rating=g&lang=es`,
   }).done( function (results) {
-    results.data;
+    $(".results").append(`
+      <h2> Resultados de búsqueda de: ${searchInputData} </h2>
+    `)
+    var data = results.data;
+    for ( const gif of data) {
+      var url = gif.images.downsized_medium.url
+      $(".results").append(`
+      <img src="${url}">
+      `)
+    }
   });
 });
